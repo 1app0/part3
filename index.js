dca8f69d1963c5c243dbfa0fd2bade451dee9afb
Alex Vasilianov <293173@via.dk>
2021-07-12 22:22:31 +0200

    Fixed a small bug

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.post(`${baseUri}`, (request, response) => {
   response.json(person)
 })
 
-const PORT = proccess.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
